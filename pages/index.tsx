56eefb7395abc725a2760405eea259a9be736b66
Andrea <andreabarberio@MBP-di-Andrea.station>
2022-11-24 11:02:43 +0100

    feat: create nested paths and 404 page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,20 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
-export default function Home() {
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
+export const HomePage = () => {
   return (
     <div className={styles.container}>
-    <h1>Hello Next World!!!</h1>
+      <h1>Hello Next World!!!</h1>
+      <ul>
+        <li>
+          <Link href="/portfolio">Portfolio</Link>
+        </li>
+        <li>
+          <Link href="/clients">Clients</Link>
+        </li>
+      </ul>
     </div>
-  )
-}
+  );
+};
+export default HomePage;
